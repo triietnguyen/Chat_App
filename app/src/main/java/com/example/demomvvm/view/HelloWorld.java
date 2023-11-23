f7d07db65098fdb34cb56d3bc14b300b87fe8038
Triet Nguyen <trietnguyenhoangminh@gmail.com>
2023-11-23 17:01:17 +0700

    done

--- app/src/main/java/com/example/demomvvm/view/HelloWorld.java
+++ app/src/main/java/com/example/demomvvm/view/HelloWorld.java
@@ -12,6 +12,6 @@ public class HelloWorld extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.helloworld);
+        setContentView(R.layout.activity_game);
     }
 }
