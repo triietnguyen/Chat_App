430b840495aac9e8a440fd89456e03e222f6122e
Triet Nguyen <trietnguyenhoangminh@gmail.com>
2023-11-30 10:45:51 +0700

    fix done

--- app/src/main/java/com/example/demomvvm/view/MessagesActivity.java
+++ app/src/main/java/com/example/demomvvm/view/MessagesActivity.java
@@ -1,2 +1,17 @@
-package com.example.demomvvm.view;public class MessagesActivity {
+package com.example.demomvvm.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.demomvvm.R;
+
+public class MessagesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messageslist);
+
+    }
 }
